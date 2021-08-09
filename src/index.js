b1752bd1fb0a7bade90f03694381cbfacf82bc29
Alexspill <almi78@mail.ru>
2021-08-09 18:57:35 +0500

    temporary

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function toReadable (number) {
-  
+    let arr = number.toString().split('').map(Number);
+    arrl = arr.length;
+    const units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const dec2 = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const decs = ['', '', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    if (arrl > 3 || arrl < 0) 
+        {
+            return 'Too big or too small number';
+        }
+    else if (arrl === 0) 
+        {
+            return 'zero';
+        }
+    else if (arrl === 1) 
+        {
+          return units[arr[arrl-1]];
+    }
 }
